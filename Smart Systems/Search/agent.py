b7e3c5f3458bbac1d006a54f5ac4986d3139442e
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-04-16 11:40:31 -0500

    Adding tests

--- Smart Systems/Search/agent.py
+++ Smart Systems/Search/agent.py
@@ -66,16 +66,46 @@ class Agent:
         pass
 
     def simulate_A_star(self):
-        pass
+        queue = deque()
+        ordered_queue = deque()
+        path = {}
+        done = False
+        goal_pos = self.pos_2_vec(self.env.maze_view.goal)
+        initial_pos = self.env.maze_view._MazeView2D__robot
+        # cost = cant_steps needed to get here
+        cost = 0
+        heuristic = abs(initial_pos[0] - goal_pos[0]) + \
+            abs(initial_pos[1] - goal_pos[1])
+        real_cost = heuristic + cost
+        node_initial_pos = [initial_pos, real_cost]
+        ordered_queue.append(node_initial_pos)
+        queue.append(initial_pos)
+        path[self.pos_2_vec(initial_pos)] = [None, heuristic, cost, real_cost]
+        while not done:
+            if len(ordered_queue) > 0:
+                queue = self.posible_next_steps_ordered(ordered_queue)
+            node = queue.pop()
+            ordered_queue.pop()
+            # Check if the cuurrentt node is the goal
+            if all(node == self.env.maze_view.goal):
+                done = True
+                return path
+            self.move_robot(node)
+            self.handle_neighboors_A_star(node, path, queue, ordered_queue)
+
+            if self.render:
+                self.env.maze_view.update()
+                time.sleep(.1)
 
     def pos_2_vec(self, pos):
         return (pos[0], pos[1])
 
     def handle_neighboors(self, current, path, queue):
+        goal = self.env.maze_view.goal
         for move in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
             next_move = [current[0] + move[0], current[1] + move[1]]
             # Valide borders
-            if next_move[0] < 0 or next_move[0] > 9 or next_move[1] < 0 or next_move[1] > 9:
+            if next_move[0] < 0 or next_move[0] > goal[0] or next_move[1] < 0 or next_move[1] > goal[1]:
                 continue
 
             self.move_robot(next_move, set_pos=False)
@@ -89,6 +119,40 @@ class Agent:
             if valid and self.pos_2_vec(next_move) not in path and next_move not in queue:
                 queue.append(next_move)
                 path[self.pos_2_vec(next_move)] = self.pos_2_vec(current)
+
+    def handle_neighboors_A_star(self, current, path, queue, ordered_queue):
+        posible_move = deque()
+        goal = self.env.maze_view.goal
+        if len(ordered_queue) > 0:
+            queue = self.posible_next_steps_ordered(ordered_queue)
+
+        for move in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+            next_move = [current[0] + move[0], current[1] + move[1]]
+            # Valide borders
+            if next_move[0] < 0 or next_move[0] > goal[0] or next_move[1] < 0 or next_move[1] > goal[1]:
+                continue
+            self.move_robot(next_move, set_pos=False)
+            new_pos = self.env.maze_view._MazeView2D__robot
+            # A move was valid if pos changed
+            # valid = not all(current == new_pos)
+            valid = self.is_open(current, next_move, move)
+            # Return to the current
+            self.move_robot(current)
+            if valid and self.pos_2_vec(next_move) not in path and next_move not in queue:
+                cant_steps_father = path.get(self.pos_2_vec(current))[1]
+                node_next_move = [next_move, cant_steps_father]
+                ordered_queue.append(node_next_move)
+                queue.append(next_move)
+                goal_pos = self.pos_2_vec(self.env.maze_view.goal)
+                # actual_pos = current
+                actual_pos = next_move
+                # cost = cant_steps needed to get here
+                cost = path.get(self.pos_2_vec(current))[2] + 1
+                heuristic = abs(actual_pos[0] - goal_pos[0]) + \
+                    abs(actual_pos[1] - goal_pos[1])
+                real_cost = heuristic + cost
+                path[self.pos_2_vec(next_move)] = (self.pos_2_vec(
+                    current), heuristic, cost, real_cost)
 
     def move_robot(self, dir, set_pos=True):
         # update the drawing
@@ -111,13 +175,18 @@ class Agent:
             self.env.maze_view.update()
 
     def recover_solution_path(self, path):
-        final_pos = (9, 9)
+        final_pos = self.pos_2_vec(self.env.maze_view.goal)
         shortest_path = deque()
         shortest_path.append(final_pos)
         ancestor = path[final_pos]
+        if len(ancestor) > 2:
+            ancestor = ancestor[0]
+
         while ancestor:
             shortest_path.appendleft(ancestor)
             ancestor = path[ancestor]
+            if ancestor and len(ancestor) > 2:
+                ancestor = ancestor[0]
         return shortest_path
 
     def is_open(self, current, next_move, move):
@@ -147,5 +216,32 @@ class Agent:
         path = list(path)
         for cell in path:
             self.env.maze_view._MazeView2D__colour_cell(
-                cell, colour=(160, 60, 60), transparency=240)
+                cell, colour=(252, 186, 2), transparency=240)
             self.env.render()
+
+    def posible_next_steps_ordered(self, q):
+        c = int(0)
+        tam = len(q)
+        vec = [0 for x in range(len(q))]
+        while q:
+            vec[c] = q.popleft()
+            c = c + 1
+
+        # ordenarmiento burbuja
+        band = int(0)
+        while band == 0:
+            band = 1
+            for i in range(0, tam - 1):
+                if(vec[i][0] > vec[i+1][0]):
+                    aux = vec[i+1][0]
+                    vec[i+1][0] = vec[i][0]
+                    vec[i][0] = aux
+                    band = 0
+
+        for i in range(0, tam):
+            q.append(vec[i])
+
+        queue = deque()
+        for node in q:
+            queue.append(node[0])
+        return queue
--- Smart Systems/Search/main.py
+++ Smart Systems/Search/main.py
@@ -1,19 +1,86 @@
 import time
+import sys
 
 import pygame
 import gym
 import gym_maze
+import matplotlib.pyplot as plt
 
 from agent import Agent
+from memory_profiler import memory_usage
+
+
+def test(search_type, with_portals=False, test_mem=False):
+    NUM_TEST = 1
+    speed = {}
+    memory = {}
+
+    total = 0
+    mem_total = []
+    for x, y in [(3, 3), (5, 5), (10, 10), (20, 20), (30, 30), (100, 100)]:
+        total = 0
+        mem_total = []
+        for _ in range(NUM_TEST):
+            # Initialize the "maze" environment
+            portals = "-plus" if with_portals or x in [20, 30] else ""
+            env = gym.make(f"maze-random-{x}x{y}{portals}-v0")
+
+            agent = Agent(env, type_search=search_type, render=False, debug=False)
+
+            if test_mem:
+                start = time.time()
+                mem_usage = memory_usage(agent.process)
+                end = time.time()
+                mem_total.extend(mem_usage)
+            else:
+                start = time.time()
+                agent.process()
+                end = time.time()
+
+            total += end - start
+            
+
+        speed[x] = total / NUM_TEST
+        memory[x] = max(mem_total) if mem_total else []
+
+    return speed, memory
 
 
 if __name__ == "__main__":
 
     # Initialize the "maze" environment
-    env = gym.make("maze-random-10x10-plus-v0")
+    # env = gym.make("maze-random-100x100-v0")
 
-    agent = Agent(env, type_search=Agent.SearchTypes.BREATH_FIRST_SEARCH)
+    # agent = Agent(env, type_search=Agent.SearchTypes.A_STAR, debug=True)
 
-    agent.process()
+    # agent.process()
 
-    time.sleep(10)
+    # Test
+    results, _ = test(Agent.SearchTypes.BREATH_FIRST_SEARCH)
+    results_2, _ = test(Agent.SearchTypes.A_STAR)
+
+    plt.plot(list(results.keys()), list(results.values()), 'r', label="BFS")
+    plt.plot(list(results_2.keys()), list(results_2.values()), 'b', label="A*")
+    plt.xlabel('Grid Size (N)')
+    plt.ylabel('Seconds')
+    plt.legend(loc="upper left")
+    plt.grid()
+    plt.title("Time Used")
+    plt.show()
+
+    _, memory = test(Agent.SearchTypes.BREATH_FIRST_SEARCH, test_mem=True)
+    _, memory_2 = test(Agent.SearchTypes.A_STAR, test_mem=True)
+
+    plt.plot(list(memory.keys()), list(memory.values()), 'r', label="BFS")
+    plt.plot(list(memory_2.keys()), list(memory_2.values()), 'b', label="A*")
+    plt.xlabel('Grid Size (N)')
+    plt.ylabel('Memory (MiB)')
+    plt.legend(loc="upper left")
+    plt.grid()
+    plt.title("Memory Used")
+    plt.show()
+
+    # mem_usage = memory_usage(agent2.process)
+    # print('Maximum memory usage: %s' % max(mem_usage))
+
+    time.sleep(5)
