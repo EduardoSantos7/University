095aa46b693f575d545cba294864138b2d7bef12
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-05-31 19:36:08 -0500

    Get license in parallel

--- Computer Vision/ocr.py
+++ Computer Vision/ocr.py
@@ -12,44 +12,7 @@ def get_license_plate():
     image = cv2.imread('toyota.jpg')
     image = cv2.resize(image, (800, 800))
     gray, canny = Preprocessor.gray_blur_canny(image)
-
-    _, cnts, _ = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-    for c in cnts:
-        area = cv2.contourArea(c)
-
-        x, y, w, h = cv2.boundingRect(c)
-        epsilon = 0.09*cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, epsilon, True)
-
-        if len(approx) == 4 and area > 8000:
-            print('area=', area)
-            # cv2.drawContours(image,[approx],0,(0,255,0),3)
-
-            aspect_ratio = float(w)/h
-
-            if aspect_ratio > 2.2:
-                placa = gray[y:y+h, x:x+w]
-                text = pytesseract.image_to_string(placa, config='--psm 7')
-                text2 = pytesseract.image_to_string(placa, config='--psm 6')
-                text3 = pytesseract.image_to_string(placa, config='--psm 11')
-                text4 = pytesseract.image_to_string(placa, config='--psm 13')
-                print('PLACA: ', extract_license(text))
-
-                # cv2.imshow('PLACA', placa)
-                # cv2.moveWindow('PLACA', 780, 10)
-                # cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 3)
-                # cv2.putText(image, text, (x-20, y-10), 1, 2.2, (0, 255, 0), 3)
-
-    # cv2.imshow('Image', image)
-    # cv2.moveWindow('Image', 45, 10)
-    # cv2.waitKey(0)
-
-
-def get_license_plate_p():
-    image = cv2.imread('toyota.jpg')
-    image = cv2.resize(image, (800, 800))
-    gray, canny = Preprocessor.gray_blur_canny(image)
+    text_freq = {}
 
     _, cnts, _ = cv2.findContours(
         canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -57,13 +20,11 @@ def get_license_plate_p():
     for c in cnts:
         area = cv2.contourArea(c)
 
-        x, y, w, h = cv2.boundingRect(c)
         epsilon = 0.09*cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, epsilon, True)
-
+        # Process a rectangule
         if len(approx) == 4 and area > 8000:
-            print('area=', area)
-            # cv2.drawContours(image,[approx],0,(0,255,0),3)
+            x, y, w, h = cv2.boundingRect(c)
 
             aspect_ratio = float(w)/h
 
@@ -74,45 +35,18 @@ def get_license_plate_p():
                     results = [executor.submit(orc, placa, psm) for psm in psms]
 
                     for f in as_completed(results):
-                        if f.result():
-                            print('PLACA: ', extract_license(f.result()))
+                        license_plate = extract_license(f.result())
+                        if license_plate:
+                            text_freq[license_plate] = text_freq.get(license_plate, 0) + 1
 
-                # cv2.imshow('PLACA', placa)
-                # cv2.moveWindow('PLACA', 780, 10)
-                # cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 3)
-                # cv2.putText(image, text, (x-20, y-10), 1, 2.2, (0, 255, 0), 3)
+                license_plate = max(text_freq, key=text_freq.get)
 
+    return license_plate
 
-    # cv2.imshow('Image', image)
-    # cv2.moveWindow('Image', 45, 10)
-    # cv2.waitKey(0)
-
-
-def op(c, gray):
-    area = cv2.contourArea(c)
-
-    x, y, w, h = cv2.boundingRect(c)
-    epsilon = 0.09*cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, epsilon, True)
-
-    if len(approx) == 4 and area > 8000:
-        print('area=', area)
-        # cv2.drawContours(image,[approx],0,(0,255,0),3)
-
-        aspect_ratio = float(w)/h
-
-        if aspect_ratio > 2.2:
-            placa = gray[y:y+h, x:x+w]
-            with ProcessPoolExecutor() as executor:
-                psms = ['--psm 7', '--psm 6', '--psm 11', '--psm 13']
-                results = [executor.submit(orc, placa, psm) for psm in psms]
-
-                for f in as_completed(results):
-                    if f.result():
-                        print('PLACA: ', extract_license(f.result()))
 
 def orc(image, psm):
     return pytesseract.image_to_string(image, config=psm)
+
 
 s = time.time()
 get_license_plate()
--- Computer Vision/utils/ocr/license.py
+++ Computer Vision/utils/ocr/license.py
@@ -2,6 +2,8 @@ import re
 
 
 def extract_license(text: str):
+    if not text:
+        return ""
     text = text.strip()
     pattern = re.compile(r'[0-9A-Z\-\s]+')
     matches = pattern.findall(text)
